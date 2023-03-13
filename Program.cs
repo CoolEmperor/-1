1b2f0c8b02b6d5f36f2723fb14393cfb1766db06
SuperDima228 <dmitrybugai.live@gmail.com>
2023-03-13 09:02:53 +0300

    Versia 1.3

--- Program.cs
+++ Program.cs
@@ -8,10 +8,24 @@ namespace Practica
 {
     class Weapon
     {
-        string name;
-        float caliber;
-        double range;
-
-
+        public string name;
+        public float caliber;
+        public double range;
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon();
+            weapon.name = " ";
+            weapon.caliber = 2.3f;
+            weapon.range = 1.1d;
+
+            Console.WriteLine(weapon.name);
+            Console.WriteLine(weapon.caliber);
+            Console.WriteLine(weapon.range);
+        }
+    }
+
+    
 }
